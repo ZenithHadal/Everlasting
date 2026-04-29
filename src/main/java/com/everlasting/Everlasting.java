84fac444843f5e17873734f79563c4b2de45aab2
crystove <zenithhadal@gmail.com>
2026-04-29 13:30:16 -0300

    29/4/26

--- src/main/java/com/everlasting/Everlasting.java
+++ src/main/java/com/everlasting/Everlasting.java
@@ -1,5 +1,6 @@
 package com.everlasting;
 
+import com.everlasting.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,8 @@ public class Everlasting implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
---
+++ src/main/java/com/everlasting/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.everlasting.item;
+
+import com.everlasting.Everlasting;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PYRORITA = registerItem("pyro_ore", new Item(new Item.Settings()));
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Everlasting.MOD_ID, name), item);
+    }
+
+  public static void registerModItems() {
+      Everlasting.LOGGER.info("Registering Mod Items //" + Everlasting.MOD_ID);
+
+
+      ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+      entries.add(PYRORITA);
+
+      });
+  }
+}
